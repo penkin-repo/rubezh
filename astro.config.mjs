bd8a09891cf4e994caf4f7a551a0a271a98e8d3b
Alexander Penkin <iampenkin@gmail>
2026-03-03 11:47:39 +0300

    Changes to be committed: 	modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,13 @@ import keystatic from '@keystatic/astro';
 import icon from 'astro-icon';
 
 // https://astro.build/config
+// https://github.com/penkin-repo/rubezh
 // SSG режим (статическая генерация) — адаптер не нужен для dev и статики
 // node adapter добавить при необходимости деплоя на Node.js сервер
 export default defineConfig({
   output: 'static',
+  base: '/rubezh',
+  site: 'https://penkin-repo.github.io/rubezh',
   vite: {
     plugins: [tailwindcss()]
   },
