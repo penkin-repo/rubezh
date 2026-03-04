7b80ea121c44620888de992a46d3389cebb2ec9a
Alexander Penkin <iampenkin@gmail>
2026-03-04 21:47:06 +0300

    Changes to be committed: 	modified:   astro.config.mjs 	modified:   package.json 	modified:   pnpm-lock.yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
-import vercel from '@astrojs/vercel';
 import icon from 'astro-icon';
-// https://astro.build/config
-// https://github.com/penkin-repo/rubezh
-// SSG режим (статическая генерация) — адаптер не нужен для dev и статики
-// node adapter добавить при необходимости деплоя на Node.js сервер
 
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   // base: '/rubezh',
   adapter: vercel(),
   // site: 'https://penkin-repo.github.io',
